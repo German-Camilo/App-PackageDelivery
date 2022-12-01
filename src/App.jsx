e2d2b3874c40f44416f2d025b5648e74e66195e7
narcilap <potosi@uninorte.edu.co>
2022-11-30 21:46:28 -0500

    - Register view was created and linked with the register button

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}></Route>
         <Route path='/dashboard' element={<h1>Dashboard</h1>}></Route>
+        <Route path='/Register' element={<Register/>}></Route>
       </Routes>
     </BrowserRouter>
   );
